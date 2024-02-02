c350a00368f5d905f7a9733418f1bfc595f5e56d
HibaHmd <lh_hamed@esi.dz>
2024-02-02 17:02:43 +0100

    Des modifications aportées

--- main.py
+++ main.py
@@ -1,64 +1,76 @@
-# vérifier les paramètre de connexion avec BDD
-# L'adapter avec BDD
-# acceder à la partie qui contier des article vérifier d'après les modérateur 
-
-
-
-from fastapi import FastAPI , BackgroundTasks , Query
-from elasticsearch import Elasticsearch
+from fastapi import FastAPI , BackgroundTasks , Query , HTTPException
+from elasticsearch import Elasticsearch , ElasticsearchException
 from elasticsearch.helpers import bulk
 from sqlalchemy import create_engine
+import logging
+
+
+#retournant des réponses appropriées aux clients de l'API en cas d'erreur.
+logging.basicConfig(filename='app.log', level=logging.ERROR)
+
 
 # créer une instance de FastAPI
 app = FastAPI ()
 
 # Connexion à Elasticsearch
-es = Elasticsearch(['https://localhost:9200'], basic_auth=('elastic', 'ZTwHulctYUrbUtc_EA+Y'), verify_certs=False)
+try:
+    es = Elasticsearch(['https://localhost:9200'], basic_auth=('elastic', 'ZTwHulctYUrbUtc_EA+Y'), verify_certs=False)
+except ElasticsearchException as e:
+    logging.error(f"Erreur de connexion à Elasticsearch : {e}")  # enregistrer l'erreur 
+    raise HTTPException(status_code=500, detail="Erreur de connexion à Elasticsearch")  # gérere un requette Http 
+
 index_name = "index_article"
 last_indexed_id = None  # Variable pour stocker le dernier ID indexé
 
-
 # Connect to MySQL
-engine = create_engine('mysql+mysqlconnector://user:password@localhost:3306/artifind')
+try:
+    engine = create_engine('mysql+mysqlconnector://root:mariamarai@localhost:3306/artifind')
+except Exception as e:
+    logging.error(f"Erreur de connexion à MySQL : {e}")
+    raise HTTPException(status_code=500, detail="Erreur de connexion à MySQL")
 
-#fetch data from MySQL
+
+# Fonction pour récupérer les données depuis MySQL
 def fetch_data_from_mysql(last_id=None):
-    # Connect to the database
-    with engine.connect() as connection:
-        # Execute a SELECT query on the 'article' table
-        query = f'SELECT * FROM article WHERE id > {last_id}' if last_id else 'SELECT * FROM article'
-        result = connection.execute(query)
-        # Fetch all rows
-        rows = result.fetchall()
-    # Return the fetched data
-    return rows
+    try:
+        # Connect to the database
+        with engine.connect() as connection:
+            # Execute a SELECT query on the 'article' table
+            query = f'SELECT * FROM article WHERE id > {last_id}' if last_id else 'SELECT * FROM article'
+            result = connection.execute(query)
+            # Fetch all rows
+            rows = result.fetchall()
+        return rows
+    except Exception as e:
+        logging.error(f"Erreur lors de la récupération des données depuis MySQL: {e}")
+        raise HTTPException(status_code=500, detail="Erreur lors de la récupération des données depuis MySQL")
+        
 
 
-# Format data for Elasticsearch
+# Fonction pour formater les données pour Elasticsearch
 def format_data_for_elasticsearch(rows):
     formatted_data = []   # créer une liste des document récupérés de la BBD
     for row in rows:
         doc = {
-            'id':row[1] ,
-            'titre': row[2],
-            'resume': row[3],
-            'auteurs': row[4],
-            'institutions': row[5],
-            'texte_integral': row[6],
-            'date_publication': row[7],
+            'id':row[0] ,
+            'titre': row[1],
+            'resume': row[2],
+            'auteurs': row[3],
+            'institutions': row[4],
+            'texte_integral': row[5],
+            'date_publication': row[6],
         }
         formatted_data.append(doc)   # à chaque fois on recupère un document de l'article on l'ajoute à la liste
     return formatted_data   # returner la liste des document 
 
 
-# Index data in Elasticsearch
+# Fonction pour indexer les données dans Elasticsearch      data is formatted_data
 def index_data_in_elasticsearch(data):
     global last_indexed_id
     # Ensure the index exists
     if not es.indices.exists(index=index_name):
         es.indices.create(index=index_name, body={"mappings": {"properties": { "id": {"type": "int"},"titre": {"type": "text"}, "resume": {"type": "text"},  "auteurs": {"type": "keyword"}, "institutions": {"type": "keyword"}, "texte_integral": {"type": "text"}, "date_publication": {"type": "date"} }
                 }}, ignore=400)
-
     # Use the Elasticsearch bulk helper for efficient indexing (mettre les info de chaque article dans l'index)
     actions = [
         {
@@ -69,11 +81,10 @@ def index_data_in_elasticsearch(data):
         for doc in data
     ]
     es.bulk(actions=actions, index=index_name, raise_on_error=False)
-     
-    # Mettez à jour le dernier ID indexé
+    # Mise à jour le dernier ID indexé
     if actions:
         last_indexed_id = actions[-1]['_source']['id']
-    
+
 
 # Background task to  index new articles
 def index_new_articles(background_tasks: BackgroundTasks ):
@@ -84,20 +95,19 @@ def index_new_articles(background_tasks: BackgroundTasks ):
 
 
 # Fonction pour ajouter un nouvel article à la base de données
-def add_new_article_to_database(article , background_tasks: BackgroundTasks ):
+@app.post("/add_article")
+async def add_article(article, background_tasks: BackgroundTasks):
     
     # Code pour ajouter un nouvel article à la base de données
-    
-    #......
+    #.....
 
-
-    # Planifier l'indexation en arrière-plan
     background_tasks.add_task(index_new_articles)
-
-    
-    return {"message": "Article added successfully."}
+    return {"message": "Article ajouté avec succès."}
 
 
+
+cached_results = []   # variable pour stocker les résultats actuelles de la recherche 
+# chercher dans les documents de elasticSearch le mot clé dans query 
 def perform_elasticsearch_search(query: str):
 
     search_body = {
@@ -110,16 +120,23 @@ def perform_elasticsearch_search(query: str):
     }
 
     search_results = es.search(index=index_name, body=search_body)
+    hits = search_results["hits"]["hits"]
 
-    return search_results["hits"]["hits"]
+    formatted_results = [
+        {
+            "titre": hit['_source']['titre'],
+            "resume": hit['_source']['resume'],
+            "auteurs": hit['_source']['auteurs'],
+            "institutions": hit['_source']['institutions'],
+            "texte_integral": hit['_source']['texte_integral'],
+            "date_publication": hit['_source']['date_publication']
+        }
+        for hit in hits
+    ]
+    cached_results.extend(formatted_results)
 
+    return formatted_results
 
-
-
-# Affecter une requette pour ajouter un article à la base de données 
-@app.post("/add_article")
-async def add_article(article , background_tasks: BackgroundTasks):
-    return add_new_article_to_database(article)
 
 
 # Affecter une requette de recherche 
@@ -129,9 +146,84 @@ async def search_articles(query: str = Query(..., title="Recherche")):
     return {"results": search_results}
 
 
+# ***filtrer par auteur 
+@app.get("/filter_by_author")
+async def filter_by_author(author: str = Query(..., title="Author Name")):
+    # Filtrer les résultats stockés dans cached_results par auteur
+    filtered_results = [
+        {
+            "titre": result['titre'],
+            "resume": result['resume'],
+            "auteurs": result['auteurs'],
+            "institutions": result['institutions'],
+            "texte_integral": result['texte_integral'],
+            "date_publication": result['date_publication']
+        }
+        for result in cached_results if author in result['auteurs']
+    ]
+    cached_results[:] = filtered_results  # Mise à jour de cached_results avec les nouveaux résultats
+    return filtered_results
+
+# ***filtrer les résultats de la recherche par institution 
+@app.get("/filter_by_institution")
+async def filter_by_institution(institution: str = Query(..., title="Institution Name")):
+    # Filtrer les résultats stockés dans cached_results par institution
+    filtered_results = [
+        {
+            "titre": result['titre'],
+            "resume": result['resume'],
+            "auteurs": result['auteurs'],
+            "institutions": result['institutions'],
+            "texte_integral": result['texte_integral'],
+            "date_publication": result['date_publication']
+        }
+        for result in cached_results if institution in result['institutions']
+    ]
+    cached_results[:] = filtered_results  # Mise à jour de cached_results avec les nouveaux résultats
+    return filtered_results
+
+# ***filtrer les résultats de la reccherche selon date de publication 
+@app.get("/filter_by_publication_date")
+async def filter_by_publication_date(publication_date: str = Query(..., title="Publication Date")):
+    # Filtrer les résultats stockés dans cached_results par date de publication
+    filtered_results = [
+        {
+            "titre": result['titre'],
+            "resume": result['resume'],
+            "auteurs": result['auteurs'],
+            "institutions": result['institutions'],
+            "texte_integral": result['texte_integral'],
+            "date_publication": result['date_publication']
+        }
+        for result in cached_results if publication_date == result['date_publication']
+    ]
+    cached_results[:] = filtered_results  # Mise à jour de cached_results avec les nouveaux résultats
+    return filtered_results
+
+
+#***filtere les résultats de la recherche selon les mots clés 
+@app.get("/filter_by_keyword")
+async def filter_by_keyword(keyword: str = Query(..., title="Keyword")):
+    # Filtrer les résultats stockés dans cached_results par mot-clé dans le champ texte_integral
+    filtered_results = [
+        {
+            "titre": result['titre'],
+            "resume": result['resume'],
+            "auteurs": result['auteurs'],
+            "institutions": result['institutions'],
+            "texte_integral": result['texte_integral'],
+            "date_publication": result['date_publication']
+        }
+        for result in cached_results if keyword in result['texte_integral']
+    ]
+    cached_results[:] = filtered_results  # Mise à jour de cached_results avec les nouveaux résultats
+    return filtered_results
+
+
 
 # La fonction main 
 def main () :
+
    # Initial indexing when the application starts
     background_tasks = BackgroundTasks()
     index_new_articles(background_tasks)
